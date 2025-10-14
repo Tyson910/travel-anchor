bbc9281e4815341e94838403d893d564ebce7053
Tyson <TysonS.417@gmail.com>
2025-10-13 20:41:42 -0700

    set up hono client

---
+++ nuxt-app/app/composables/use-hono-client.ts
@@ -0,0 +1,7 @@
+import type { App } from "@travel-anchor/hono-api";
+import { hc } from "hono/client";
+
+export const useHonoClient = () => {
+  const { SITE_URL } = useRuntimeConfig().public;
+  return hc<App>(`${SITE_URL}/bread`);
+};
