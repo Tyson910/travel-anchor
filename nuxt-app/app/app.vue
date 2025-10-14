07d2b4b0d50b7f3ef03a4827639d2e49a29ae353
Tyson <TysonS.417@gmail.com>
2025-10-13 20:22:40 -0700

    init nuxt app

---
+++ nuxt-app/app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <Body>
+    <NuxtRouteAnnouncer />
+    <UApp>
+      <UHeader />
+      <UMain>
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </UMain>
+      <UFooter />
+    </UApp>
+  </Body>
+</template>
---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/global.css"],
+  devServer: {
+    port: 3001,
+  },
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/test-utils"],
+});
