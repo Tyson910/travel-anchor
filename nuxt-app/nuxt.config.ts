f37522617086b966ba025de210e6557f36a22ea4
Tyson <TysonS.417@gmail.com>
2025-10-14 16:28:47 -0700

    add leaflet css file

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ["~/assets/css/global.css"],
+  css: ["~/assets/css/global.css", "leaflet/dist/leaflet.css"],
   devServer: {
     port: 3001,
   },
