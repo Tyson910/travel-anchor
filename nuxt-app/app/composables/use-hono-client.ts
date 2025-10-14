b5caf5c9bc908931252be1587a9b39847edc56bb
Tyson <TysonS.417@gmail.com>
2025-10-14 16:31:15 -0700

    set up api proxy

--- nuxt-app/app/composables/use-hono-client.ts
+++ nuxt-app/app/composables/use-hono-client.ts
@@ -1,7 +1,8 @@
 import type { App } from "@travel-anchor/hono-api";
+
 import { hc } from "hono/client";
 
 export const useHonoClient = () => {
   const { SITE_URL } = useRuntimeConfig().public;
-  return hc<App>(`${SITE_URL}/bread`);
+  return hc<App>(`${SITE_URL}/backend`);
 };
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,16 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  css: ["~/assets/css/global.css", "leaflet/dist/leaflet.css"],
-  devServer: {
-    port: 3001,
-  },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "@nuxt/ui",
-    "@nuxt/test-utils",
-    "@vueuse/nuxt",
-  ],
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	css: ["~/assets/css/global.css", "leaflet/dist/leaflet.css"],
+	devServer: {
+		port: 3001,
+	},
+	runtimeConfig: {
+		API_ENDPOINT_URL: import.meta.env.API_ENDPOINT_URL,
+	},
+	modules: [
+		"@nuxt/eslint",
+		"@nuxt/image",
+		"@nuxt/ui",
+		"@nuxt/test-utils",
+		"@vueuse/nuxt",
+	],
+	routeRules: {
+		"/backend/**": {
+			proxy: {
+				to: `${import.meta.env.API_ENDPOINT_URL}/**`,
+				headers: {
+					// "Content-Security-Policy":
+					// 	"default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';",
+					"Referrer-Policy": "strict-origin-when-cross-origin",
+					"Strict-Transport-Security":
+						"max-age=31536000; includeSubDomains; preload",
+					"X-Content-Type-Options": "nosniff",
+					"X-Frame-Options": "DENY",
+				},
+			},
+		},
+	},
+	$development: {
+		runtimeConfig: {
+			public: {
+				SITE_URL: "http://localhost:3001",
+			},
+		},
+	},
+	$production: {
+		runtimeConfig: {
+			public: {
+				SITE_URL: "https://flightanchor.com",
+			},
+		},
+	},
 });
