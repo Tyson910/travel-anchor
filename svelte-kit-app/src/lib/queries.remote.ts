6fbd2d6c16d6d4719a3e01e3fbccbd068c0659d9
Tyson <TysonS.417@gmail.com>
2025-10-15 15:09:59 -0700

    add remote queries

---
+++ svelte-kit-app/src/lib/queries.remote.ts
@@ -0,0 +1,70 @@
+import type { App } from "@travel-anchor/hono-api";
+
+import { hc, parseResponse, DetailedError } from "hono/client";
+import * as z from "zod";
+
+import { query } from "$app/server";
+
+const useHonoClient = () => {
+	return hc<App>(`http://localhost:3000`);
+};
+
+const airportIATAValidator = z.string().length(3).toUpperCase();
+
+export const useAirportSearchQuery = query(z.string(), async (searchQuery) => {
+	const endpoint = useHonoClient().v1.airport.$get({
+		query: {
+			query: searchQuery,
+		},
+	});
+
+	const result = await parseResponse(endpoint).catch((e: DetailedError) => e);
+
+	if (result instanceof DetailedError) {
+		throw DetailedError;
+	} else if (result instanceof Error) {
+		throw result;
+	}
+
+	return result;
+});
+
+export const useAirportDetailsQuery = query(
+	airportIATAValidator,
+	async (airportIATA) => {
+		const endpoint = useHonoClient().v1.airport[":IATA"].$get({
+			param: {
+				IATA: airportIATA,
+			},
+		});
+
+		const result = await parseResponse(endpoint).catch((e: DetailedError) => e);
+
+		if (result instanceof DetailedError) {
+			throw DetailedError;
+		} else if (result instanceof Error) {
+			throw result;
+		}
+
+		return result;
+	},
+);
+
+export const useFlightRouteQuery = query(
+	z.array(airportIATAValidator).min(1),
+	async (iataCodes) => {
+		const endpoint = useHonoClient().v1["flight-route"].$get({
+			query: { IATA: iataCodes },
+		});
+
+		const result = await parseResponse(endpoint).catch((e: DetailedError) => e);
+
+		if (result instanceof DetailedError) {
+			throw DetailedError;
+		} else if (result instanceof Error) {
+			throw result;
+		}
+
+		return result;
+	},
+);
