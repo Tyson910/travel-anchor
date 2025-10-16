c555c8df45438ace910464a67055e59a1219b316
Tyson <TysonS.417@gmail.com>
2025-10-16 12:07:13 -0700

    update button component

--- svelte-kit-app/src/lib/components/ui/button/index.ts
+++ svelte-kit-app/src/lib/components/ui/button/index.ts
@@ -1,9 +1,17 @@
-import Button from "./button.svelte";
-
-export { Button };
-
-export {
+import Root, {
+	type ButtonProps,
 	type ButtonSize,
 	type ButtonVariant,
 	buttonVariants,
 } from "./button.svelte";
+
+export {
+	Root,
+	type ButtonProps as Props,
+	//
+	Root as Button,
+	buttonVariants,
+	type ButtonProps,
+	type ButtonSize,
+	type ButtonVariant,
+};
