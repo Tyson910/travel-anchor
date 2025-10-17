f4724e5e584cb07267b9551ff24265491a41ba43
Tyson <TysonS.417@gmail.com>
2025-10-16 20:18:21 -0700

    set up tw

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -3,9 +3,14 @@
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	server: {
+		port: 3000,
+	},
+	vite: { plugins: [tailwindcss()] },
 	integrations: [vue({ devtools: true }), svelte(), react()],
 });
