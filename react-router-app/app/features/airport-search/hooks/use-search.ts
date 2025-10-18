048204b85157de62958dbeff686121540fa1b96e
Tyson <TysonS.417@gmail.com>
2025-10-18 14:30:37 -0700

    allow consumers to pass in swr options

--- react-router-app/app/features/airport-search/hooks/use-search.ts
+++ react-router-app/app/features/airport-search/hooks/use-search.ts
@@ -3,7 +3,7 @@ import {
 	type InferResponseType,
 	parseResponse,
 } from "hono/client";
-import useSWR from "swr";
+import useSWR, { type SWRConfiguration } from "swr";
 
 import { honoClient } from "~/lib/hono-client";
 
@@ -12,7 +12,10 @@ export type AirportSearchQueryResult = InferResponseType<
 	typeof airportEndpoint
 >;
 
-export const useAirportSearchQuery = (searchQuery: string) => {
+export const useAirportSearchQuery = (
+	searchQuery: string,
+	opts?: SWRConfiguration<AirportSearchQueryResult, DetailedError>,
+) => {
 	const fetcher = async (searchTerm: string) => {
 		const result = await parseResponse(
 			airportEndpoint({
@@ -24,7 +27,11 @@ export const useAirportSearchQuery = (searchQuery: string) => {
 		return result;
 	};
 
-	return useSWR<AirportSearchQueryResult, DetailedError>(searchQuery, fetcher);
+	return useSWR<AirportSearchQueryResult, DetailedError>(
+		searchQuery,
+		fetcher,
+		opts,
+	);
 };
 
 // export const useFlightRoutesQuery = (iataCode: string) => {
