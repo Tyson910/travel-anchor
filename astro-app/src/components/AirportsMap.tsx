d566137fa376a4db04e335759aa30c4f90025ca7
Tyson <TysonS.417@gmail.com>
2025-10-17 21:08:29 -0700

    first pass at AirportsMap

---
+++ astro-app/src/components/AirportsMap.tsx
@@ -0,0 +1,54 @@
+import type { LatLngExpression, Map as LeafletMap } from "leaflet";
+
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "@/styles/custom-leaftlet.css";
+
+import { useEffect } from "react";
+
+interface Airport {
+	iata_code: string;
+	latitude: number;
+	longitude: number;
+	name?: string;
+	city_name?: string | null;
+}
+
+interface AirportsMapProps {
+	airports: Airport[];
+}
+
+export function AirportsMap({ airports }: AirportsMapProps) {
+	useEffect(() => {
+		fitBoundsToRoutes(airports);
+	}, [airports]);
+	return (
+		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={[51.505, -0.09]}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</MapContainer>
+	);
+}
+
+function fitBoundsToRoutes(airports: Airport[]) {
+	if (airports.length === 0) return;
+
+	const map = useMap();
+	const bounds: [number, number][] = [];
+
+	airports.forEach((airport) => {
+		bounds.push([airport.latitude, airport.longitude]);
+	});
+
+	if (bounds.length > 0) {
+		map.fitBounds(bounds, { padding: [20, 20] });
+	}
+}
