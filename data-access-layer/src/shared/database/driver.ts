e972144b68eed5eec291fd7bd6299cbdb969d56b
Tyson <TysonS.417@gmail.com>
2025-10-19 14:12:05 -0700

    Parse numeric types as numbers

--- data-access-layer/src/shared/database/driver.ts
+++ data-access-layer/src/shared/database/driver.ts
@@ -1,10 +1,13 @@
 import type { DB } from "./types.ts";
 
 import { Kysely, ParseJSONResultsPlugin, PostgresDialect } from "kysely";
-import { Pool } from "pg";
+import pg from "pg";
+
+// Parse numeric types as numbers
+pg.types.setTypeParser(1700, (val) => parseFloat(val));
 
 const dialect = new PostgresDialect({
-	pool: new Pool({
+	pool: new pg.Pool({
 		connectionString: import.meta.env.DATABASE_URL,
 		max: 10,
 	}),
