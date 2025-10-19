0510a62590561917d3e0c96414df4c3914d52656
Tyson <TysonS.417@gmail.com>
2025-10-19 14:53:43 -0700

    replace SQLite error with pg error

--- data-access-layer/src/shared/utils/errors.ts
+++ data-access-layer/src/shared/utils/errors.ts
@@ -1,180 +1,118 @@
-import type { SQLiteError } from "bun:sqlite";
-
-import { logger } from "#logger";
+import { DatabaseError as PostgresError } from "pg";
 
 /**
- * Handles SQLite database errors and maps them to appropriate HTTP responses.
- *
- * This function should only be used with instances of SQLite errors.
+ * Handles PostgreSQL database errors, logs them, and maps them to appropriate HTTP responses.
+ * This function should only be used with instances of `DatabaseError` from `pg-protocol`.
  * It returns an object containing an HTTP status code, a user-friendly message,
  * and a log level for appropriate logging.
- *
+ * All PG error codes are available at [the postgres errors appendix](https://www.postgresql.org/docs/current/errcodes-appendix.html)
  * @param {Object} error - The database error object.
- * @param {string} [error.message] - The SQLite error message.
- * @returns {{ statusCode: number; message: string; logLevel: 'warning' | 'error' | 'info' }}
- * An object containing the HTTP status code, message, and log level.
+ * @param {string} [error.code] - The PostgreSQL error code.
+ * @returns {{ statusCode: number; message: string; logLevel: 'warning' | 'error' }} An object containing the HTTP status code, message, and log level.
+ *
  */
-export function handleSQLiteError(error: SQLiteError): {
-	statusCode: number;
+export function handlePostgresError(err: PostgresError): {
+	statusCode: 400 | 403 | 409 | 500 | 503;
 	message: string;
-	logLevel: "warning" | "error" | "info";
+	logLevel: "warning" | "error";
 } {
-	type ErrorObj = Record<
-		string,
-		{
-			statusCode: number;
-			message: string;
-			logLevel: "warning" | "error" | "info";
-		}
-	>;
+	// Handle non PostgresError cases just in case we slip up somewhere
+	if (!(err instanceof PostgresError)) {
+		console.error(
+			"Unknown error instance. Please use instanceof before calling this function DatabaseError",
+			err,
+		);
+		return {
+			statusCode: 500,
+			message: "An error occurred. Please try again later.",
+			logLevel: "error",
+		};
+	}
 
-	const errorMap = {
-		SQLITE_ERROR: {
-			statusCode: 400,
-			message: "A database error occurred.",
-			logLevel: "error",
-		},
-		SQLITE_INTERNAL: {
-			statusCode: 500,
-			message: "An internal database error occurred.",
-			logLevel: "error",
-		},
-		SQLITE_PERM: {
-			statusCode: 403,
-			message: "Permission denied.",
-			logLevel: "warning",
-		},
-		SQLITE_ABORT: {
-			statusCode: 409,
-			message: "The operation was aborted.",
-			logLevel: "warning",
-		},
-		SQLITE_BUSY: {
-			statusCode: 503,
-			message: "The database is currently busy. Please try again later.",
-			logLevel: "error",
-		},
-		SQLITE_LOCKED: {
-			statusCode: 409,
-			message: "A database resource is locked. Try again later.",
-			logLevel: "error",
-		},
-		SQLITE_NOMEM: {
-			statusCode: 500,
-			message: "Insufficient memory for the operation.",
-			logLevel: "error",
-		},
-		SQLITE_READONLY: {
-			statusCode: 403,
-			message: "The database is in read-only mode.",
-			logLevel: "warning",
-		},
-		SQLITE_INTERRUPT: {
-			statusCode: 408,
-			message: "The operation was interrupted.",
-			logLevel: "warning",
-		},
-		SQLITE_IOERR: {
-			statusCode: 500,
-			message: "An input/output error occurred.",
-			logLevel: "error",
-		},
-		SQLITE_CORRUPT: {
-			statusCode: 500,
-			message: "The database is not accessible.",
-			logLevel: "error",
-		},
-		SQLITE_NOTFOUND: {
-			statusCode: 404,
-			message: "Requested database resource not found.",
-			logLevel: "warning",
-		},
-		SQLITE_FULL: {
-			statusCode: 507,
-			message: "The database has insufficient space.",
-			logLevel: "error",
-		},
-		SQLITE_CANTOPEN: {
-			statusCode: 500,
-			message: "The database could not be opened.",
-			logLevel: "error",
-		},
-		SQLITE_PROTOCOL: {
-			statusCode: 500,
-			message: "A database protocol error occurred.",
-			logLevel: "error",
-		},
-		SQLITE_EMPTY: {
-			statusCode: 204,
-			message: "The database returned no content.",
-			logLevel: "info",
-		},
-		SQLITE_SCHEMA: {
-			statusCode: 400,
-			message: "The database schema has changed.",
-			logLevel: "warning",
-		},
-		SQLITE_TOOBIG: {
-			statusCode: 413,
-			message: "The request was too large.",
-			logLevel: "warning",
-		},
-		SQLITE_CONSTRAINT: {
-			statusCode: 409,
-			message: "A database constraint was violated.",
-			logLevel: "warning",
-		},
-		SQLITE_MISMATCH: {
-			statusCode: 400,
-			message: "Invalid data format.",
-			logLevel: "warning",
-		},
-		SQLITE_MISUSE: {
-			statusCode: 500,
-			message: "The database was misused.",
-			logLevel: "error",
-		},
-		SQLITE_NOLFS: {
-			statusCode: 500,
-			message: "Large file support is unavailable.",
-			logLevel: "error",
-		},
-		SQLITE_AUTH: {
-			statusCode: 403,
-			message: "Unauthorized access.",
-			logLevel: "warning",
-		},
-		SQLITE_RANGE: {
-			statusCode: 400,
-			message: "Query parameter out of range.",
-			logLevel: "warning",
-		},
-		SQLITE_NOTADB: {
-			statusCode: 400,
-			message: "Database error occurred.",
-			logLevel: "error",
-		},
-		SQLITE_WARNING: {
-			statusCode: 200,
-			message: "A warning was logged.",
-			logLevel: "warning",
-		},
-	} as const satisfies ErrorObj;
+	const errorClass = err.code?.substring(0, 2);
 
-	const errorKeyArr = Object.keys(error) as (keyof typeof errorMap)[];
+	switch (errorClass) {
+		case "08": // Connection exceptions
+			return {
+				statusCode: 503,
+				message: "A connection issue occurred. Please try again later.",
+				logLevel: "error",
+			};
 
-	const errorKey = errorKeyArr.find((key) => error.code?.startsWith(key));
+		case "22": // Data exceptions
+			return {
+				statusCode: 400,
+				message: "Invalid data input. Please check your entries and try again.",
+				logLevel: "warning",
+			};
 
-	const response =
-		errorKey && errorMap[errorKey]
-			? errorMap[errorKey]
-			: ({
-					statusCode: 500,
-					message: "An unexpected database error occurred.",
-					logLevel: "error",
-				} as const);
+		case "23": // Integrity constraint violations
+			return {
+				statusCode: 409,
+				message: "The requested action conflicts with existing records.",
+				logLevel: "error",
+			};
 
-	logger[response.logLevel](response.message, { error: error });
+		case "42": // Syntax or access rule violations
+			return {
+				statusCode: 400,
+				message: "An error occurred while processing your request.",
+				logLevel: "error",
+			};
 
-	return response;
+		case "53": // Insufficient resources
+			return {
+				statusCode: 503,
+				message:
+					"The service is temporarily unavailable. Please try again later.",
+				logLevel: "error",
+			};
+
+		case "54": // Program limit exceeded
+			return {
+				statusCode: 400,
+				message: "The request is too complex. Please simplify and try again.",
+				logLevel: "warning",
+			};
+
+		case "57": // Operator intervention
+			return {
+				statusCode: 403,
+				message: "The operation was interrupted. Please try again.",
+				logLevel: "warning",
+			};
+
+		case "44": // WITH CHECK OPTION violation
+			return {
+				statusCode: 403,
+				message: "A security constraint prevents this action.",
+				logLevel: "warning",
+			};
+
+		case "40": // Transaction rollback
+			return {
+				statusCode: 409,
+				message: "The transaction was rolled back due to a conflict.",
+				logLevel: "warning",
+			};
+
+		//   case 'XX': // Internal errors
+		// 	return { statusCode: 500, message: 'An unexpected error occurred. Our team has been notified.', logLevel: 'error' };
+		//   case '58': // System errors
+		// 	return { statusCode: 500, message: 'A system error occurred. Please try again later.', logLevel: 'error' };
+
+		//   case 'HV': // Foreign Data Wrapper errors
+		// 	return { statusCode: 500, message: 'A foreign data error occurred. Please contact support.', logLevel: 'error' };
+
+		//   case 'F0': // Configuration file errors
+		// 	return { statusCode: 500, message: 'A configuration issue was detected. Please contact support.', logLevel: 'error' };
+
+		default:
+			return {
+				statusCode: 500,
+				message:
+					"An unexpected error occurred. Our team has been notified Please try again later.",
+				logLevel: "error",
+			};
+	}
 }
