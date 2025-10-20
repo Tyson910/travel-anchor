f02173945f017d54d672415cd06bc30f4c744ff3
Tyson <TysonS.417@gmail.com>
2025-10-19 19:06:07 -0700

    bring hono back

--- react-router-app/app/features/airport-search/hooks/use-search.ts
+++ react-router-app/app/features/airport-search/hooks/use-search.ts
@@ -1,11 +1,16 @@
+import type { App } from "@travel-anchor/hono-api";
+
 import {
 	type DetailedError,
+	hc,
 	type InferResponseType,
 	parseResponse,
 } from "hono/client";
 import useSWR, { type SWRConfiguration } from "swr";
 
-import { honoClient } from "~/lib/hono-client";
+export const honoClient = () => {
+	return hc<App>(`http://localhost:3000`);
+};
 
 const airportEndpoint = honoClient().v1.airport.$get;
 export type AirportSearchQueryResult = InferResponseType<
