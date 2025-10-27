bdf6c8e60a3289a3d7b2447006970fc755a73506
Tyson <TysonS.417@gmail.com>
2025-10-26 21:43:41 -0700

    remove unused file & dep

--- react-router-app/app/lib/hono-client.ts
+++
@@ -1,7 +0,0 @@
-import type { App } from "@travel-anchor/hono-api";
-
-import { hc } from "hono/client";
-
-export const honoClient = () => {
-	return hc<App>(import.meta.env.VITE_PUBLIC_API_URL);
-};
