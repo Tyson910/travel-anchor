651898c891844b9eb957fc412b61f136f2a410eb
Tyson <TysonS.417@gmail.com>
2025-10-27 21:12:58 -0700

    update vite server options

--- react-spa/vite.config.ts
+++ react-spa/vite.config.ts
@@ -7,6 +7,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: true,
+		port: 3001,
+		strictPort: true,
+	},
 	plugins: [
 		tanstackRouter({
 			target: "react",
