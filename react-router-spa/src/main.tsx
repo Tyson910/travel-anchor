f6c0f40d61f216448aa323e883def80898ff8b85
Tyson <TysonS.417@gmail.com>
2025-10-28 15:24:38 -0700

    set up react router routing

--- react-router-spa/src/main.tsx
+++ react-router-spa/src/main.tsx
@@ -1,11 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import "./index.css";
 
-import App from "./App.tsx";
+const root = document.getElementById("root");
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+if (!root) throw new Error("Missing root element!");
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Hello World</div>,
+	},
+]);
+
+createRoot(root).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />,
 	</StrictMode>,
 );
