37458d36abc9486eeb9cda942fae9a5236379307
Tyson <TysonS.417@gmail.com>
2025-10-28 15:54:04 -0700

    set up layout

--- react-router-spa/src/routes.tsx
+++ react-router-spa/src/routes.tsx
@@ -1,8 +1,28 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
+
+import { MinimalFooter } from "#components/layout/MinimalFooter.tsx";
+import { MinimalHeader } from "#components/layout/MinimalHeader.tsx";
 
 export const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <div className="bg-black">Hello World</div>,
+		element: (
+			<div className="min-h-screen bg-background flex flex-col">
+				<MinimalHeader />
+				<div className="flex-1">
+					<Outlet />
+				</div>
+				<MinimalFooter />
+			</div>
+		),
+		children: [
+			{
+				path: "/",
+				element: <div className="bg-black">Hello World</div>,
+			},
+			{
+				path: "/bread",
+				element: <div className="bg-blue-300">Hello World</div>,
+			},
+		],
 	},
 ]);
