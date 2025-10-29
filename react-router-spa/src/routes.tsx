8df1d093124a4888d586dfab6f6634daf72cd4be
Tyson <TysonS.417@gmail.com>
2025-10-28 18:54:00 -0700

    add hydrateFallbackElement property

--- react-router-spa/src/routes.tsx
+++ react-router-spa/src/routes.tsx
@@ -2,14 +2,10 @@ import { createBrowserRouter, Outlet, type RouteObject } from "react-router";
 
 import { MinimalFooter } from "#components/layout/MinimalFooter.tsx";
 import { MinimalHeader } from "#components/layout/MinimalHeader.tsx";
-import {
-	SearchPage,
-	searchPageLoader,
-} from "#features/airport-search/index.tsx";
-import { HomePage } from "#features/landing/route.tsx";
 
 const routes = [
 	{
+		hydrateFallbackElement: null,
 		element: (
 			<div className="min-h-screen bg-background flex flex-col">
 				<MinimalHeader />
@@ -22,12 +18,21 @@ const routes = [
 		children: [
 			{
 				path: "/",
-				Component: HomePage,
+				lazy: async () => {
+					// load component before rendering
+					const { HomePage } = await import("./features/landing/route");
+					return { Component: HomePage };
+				},
 			},
 			{
 				path: "/search",
-				loader: searchPageLoader,
-				Component: SearchPage,
+				lazy: async () => {
+					// load component and loader before rendering
+					const { SearchPage, searchPageLoader } = await import(
+						"./features/airport-search/route"
+					);
+					return { Component: SearchPage, loader: searchPageLoader };
+				},
 			},
 		],
 	},
