329ce69a2ae9bf8ccbb6041159988ad3c3a71eb6
Tyson <TysonS.417@gmail.com>
2025-10-29 22:20:56 -0700

    remove dupe iatas

--- react-spa/src/lib/validators.ts
+++ react-spa/src/lib/validators.ts
@@ -4,4 +4,6 @@ export const IATAValidator = z.string().length(3).toUpperCase();
 
 export const oneOrManyIATAValidator = z
 	.union([IATAValidator, z.array(IATAValidator)])
-	.transform((val) => (Array.isArray(val) ? val : [val]));
+	.transform((val) => (Array.isArray(val) ? val : [val]))
+	// remove dupes!
+	.transform((val) => [...new Set(val)]);
