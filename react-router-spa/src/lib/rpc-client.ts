57b155dd7f129f6fd4ad4402c9e431a5e40964c0
Tyson <TysonS.417@gmail.com>
2025-11-04 16:22:49 -0700

    migrate to tanstack

--- react-router-spa/src/lib/rpc-client.ts
+++
@@ -1,7 +0,0 @@
-import type { Router } from "@travel-anchor/data-access-layer";
-
-import { createClient } from "better-call/client";
-
-export const rpcClient = createClient<typeof Router>({
-	baseURL: `${import.meta.env.VITE_PUBLIC_API_URL}/api`,
-});
