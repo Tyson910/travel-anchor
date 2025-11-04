67c695b26f4a7ba703e9b2f87f65401ee5f7f6c0
Tyson <TysonS.417@gmail.com>
2025-11-04 12:48:31 -0700

    add wrangler compat

--- react-spa/vite.config.ts
+++ react-spa/vite.config.ts
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from "node:url";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
+		host: true,
+		port: 3001,
+		strictPort: true,
+	},
+	preview: {
 		host: true,
 		port: 3001,
 		strictPort: true,
@@ -19,6 +26,8 @@ export default defineConfig({
 		}),
 		viteReact(),
 		tailwindcss(),
+		cloudflare(),
+		devtoolsJson(),
 	],
 	resolve: {
 		alias: {
