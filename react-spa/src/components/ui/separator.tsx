4b897f3545cf0ddffcc753d3afdfd0669ade836f
Tyson <TysonS.417@gmail.com>
2025-11-05 23:56:13 -0700

    add button group component

--- react-spa/src/components/ui/separator.tsx
+++ react-spa/src/components/ui/separator.tsx
@@ -2,7 +2,7 @@
 
 import type * as React from "react";
 
-import { Separator as SeparatorPrimitive } from "radix-ui";
+import { Root as SeparatorRoot } from "@radix-ui/react-separator";
 
 import { cn } from "~/lib/utils";
 
@@ -11,15 +11,14 @@ function Separator({
 	orientation = "horizontal",
 	decorative = true,
 	...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: React.ComponentProps<typeof SeparatorRoot>) {
 	return (
-		<SeparatorPrimitive.Root
+		<SeparatorRoot
 			data-slot="separator"
 			decorative={decorative}
 			orientation={orientation}
 			className={cn(
-				"shrink-0 bg-border",
-				orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+				"bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
 				className,
 			)}
 			{...props}
