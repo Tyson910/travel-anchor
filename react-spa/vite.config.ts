7327fefb2e44ae0899f405872bebf1cf61c958cd
Tyson <TysonS.417@gmail.com>
2025-11-17 08:56:15 -0700

    remove tanstack dev tools from prod

--- react-spa/vite.config.ts
+++ react-spa/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools as tanstackDevTools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -28,6 +29,7 @@ export default defineConfig({
 		tailwindcss(),
 		cloudflare(),
 		devtoolsJson(),
+		tanstackDevTools(),
 	],
 	resolve: {
 		alias: {
